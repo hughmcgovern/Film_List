4b9e22b0484b42be083448bbc8bdcbd28a7efe16
mcgovernh <mcgovern1969@gmail.com>
2023-04-04 14:20:41 +0000

    with console log

--- mobdev-ionic-intro/src/app/pages/films/films.page.ts
+++ mobdev-ionic-intro/src/app/pages/films/films.page.ts
@@ -16,11 +16,14 @@ export class FilmsPage implements OnInit {
 
   ngOnInit() {
     this.films = this.http.get('https://swapi.dev/api/films');
-  }
+    this.films.subscribe(data =>{ console.log('my data: ', data);});
+	}
+
   openDetails(film: { url: string; }){
     let split = film.url.split('/');
     let filmId = split[split.length-2];
     this.router.navigateByUrl('/tabs/films/${filmId}');
+    
   }
   
 }
