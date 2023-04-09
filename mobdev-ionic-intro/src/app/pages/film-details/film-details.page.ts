c213e8dfc6670676151485a271104bfd87b1f0f1
hughmcgovern mcg <hugh@mcgovernconsultancy.com>
2023-04-09 16:26:21 +0000

    constructor is working

--- mobdev-ionic-intro/src/app/pages/film-details/film-details.page.ts
+++ mobdev-ionic-intro/src/app/pages/film-details/film-details.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
+
 
 @Component({
   selector: 'app-film-details',
@@ -13,21 +15,51 @@ export class FilmDetailsPage implements OnInit {
   film:any;
   crawl: any;
   filmId:any;
+  split2:any;
+  data2:any;
+  filmsdata: Observable<any>;
+  filmId4:any;
 
   constructor(private activatedRoute:ActivatedRoute, private http:HttpClient) { 
     this.filmId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.split2="hello from the constructor";
+    this.filmId=this.filmId+0;
   }
 
   ngOnInit() {
-    let id = this.activatedRoute.snapshot.paramMap.get('id');
+   /* let id = this.activatedRoute.snapshot.paramMap.get('id');
     this.http.get('https://swapi.dev/api/films/${id}').subscribe(res => {
       this.film = res;
       this.crawl = res;
       console.log("res",this.film);
       console.log("id",id);
       //this.crawl = res.results[2].opening_crawl;
-      
-    })
-  }
+      this.split2="hello";
+      this.filmId; */
 
-}
+    /* let id = this.activatedRoute.snapshot.paramMap.get('id');
+     this.http.get('https://swapi.dev/api/films/${id}').subscribe(res => {
+       this.film = res;
+       this.crawl = res;
+       console.log("res",this.film);
+       console.log("id",id);
+       //this.crawl = res.results[2].opening_crawl;
+       this.split2="hello";
+       this.filmId; */
+    //this.filmId4 = this['router'].navigateByUrl('/tabs/films/${filmId}');
+
+    this.filmsdata = this.http.get('https://swapi.dev/api/films');
+    this.filmsdata.subscribe(data2 =>{ console.log('my data: ', data2)
+    console.log("OnInit");
+    this.crawl = data2.results[0].opening_crawl;
+    console.log("*** director ***:",this.crawl);
+    console.log("****director****",data2.results[0].director);});
+    console.log("filmID4",this.filmId4);
+
+     
+      
+      
+
+  }
+}  
+
--- mobdev-ionic-intro/src/app/pages/films/films.page.ts
+++ mobdev-ionic-intro/src/app/pages/films/films.page.ts
@@ -12,15 +12,17 @@ export class FilmsPage implements OnInit {
 
   films: Observable<any>;
   crawl:any;
+  split2: any;
 
   constructor(private router: Router, private http:HttpClient) { }
 
   ngOnInit() {
     this.films = this.http.get('https://swapi.dev/api/films');
     this.films.subscribe(data =>{ console.log('my data: ', data)
-    this.crawl = data.results[2].opening_crawl;
-    console.log("*** not working crawl ***:",this.crawl);
-    console.log("****working crawl****",data.results[2].opening_crawl);
+    console.log("OnInit");
+    this.crawl = data.results[2].director;
+    console.log("*** director ***:",this.crawl);
+    console.log("****director****",data.results[2].director);
     
     
     ;});
@@ -32,21 +34,23 @@ export class FilmsPage implements OnInit {
     let split = film.url.split('/');
     let filmId = split[split.length-2];
     this.router.navigateByUrl('/tabs/films/${filmId}');
+    console.log("OpenDetails");
     console.log("filmId",filmId);
     console.log("split",split); 
     this.films = this.http.get('https://swapi.dev/api/films');
     this.films.subscribe(data =>{ console.log('my data: ', data)
-    this.crawl = data.results[filmId].opening_crawl;
+    this.crawl = data.results[filmId].director;
+    this.split2 = split;
     console.log("record 2 crawl:",this.crawl);
-    console.log("record 2 crawl again",data.results[filmId].opening_crawl);
+    console.log("record 2 crawl again",data.results[filmId].director);
   });
   }
   loaddata(){
     this.films = this.http.get('https://swapi.dev/api/films');
     this.films.subscribe(data =>{ console.log('my data: ', data)
-    this.crawl = data.results[0].opening_crawl;
-    console.log("loaddata crawl:",this.crawl);
-    console.log("loaddata crawl again",data.results[0].opening_crawl);
+    this.crawl = data.results[0].title;
+    console.log("titlel:",this.crawl);
+    console.log("titleagain",data.results[0].title);
   });
 }
   
