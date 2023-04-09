b7c0562158993b32a8de6214b2174a097e714cdb
hughmcgovern mcg <hugh@mcgovernconsultancy.com>
2023-04-09 17:05:38 +0000

    with clickable crawl

--- mobdev-ionic-intro/src/app/pages/film-details/film-details.page.ts
+++ mobdev-ionic-intro/src/app/pages/film-details/film-details.page.ts
@@ -18,48 +18,36 @@ export class FilmDetailsPage implements OnInit {
   split2:any;
   data2:any;
   filmsdata: Observable<any>;
-  filmId4:any;
+  counter:any;
+  id:any;
+  crawlany:any;
 
   constructor(private activatedRoute:ActivatedRoute, private http:HttpClient) { 
     this.filmId = this.activatedRoute.snapshot.paramMap.get('id');
-    this.split2="hello from the constructor";
-    this.filmId=this.filmId+0;
+    this.split2="hello from the constructor times three";
+    this.counter=0;
   }
 
   ngOnInit() {
-   /* let id = this.activatedRoute.snapshot.paramMap.get('id');
-    this.http.get('https://swapi.dev/api/films/${id}').subscribe(res => {
-      this.film = res;
-      this.crawl = res;
-      console.log("res",this.film);
-      console.log("id",id);
-      //this.crawl = res.results[2].opening_crawl;
-      this.split2="hello";
-      this.filmId; */
-
-    /* let id = this.activatedRoute.snapshot.paramMap.get('id');
-     this.http.get('https://swapi.dev/api/films/${id}').subscribe(res => {
-       this.film = res;
-       this.crawl = res;
-       console.log("res",this.film);
-       console.log("id",id);
-       //this.crawl = res.results[2].opening_crawl;
-       this.split2="hello";
-       this.filmId; */
-    //this.filmId4 = this['router'].navigateByUrl('/tabs/films/${filmId}');
-
     this.filmsdata = this.http.get('https://swapi.dev/api/films');
     this.filmsdata.subscribe(data2 =>{ console.log('my data: ', data2)
     console.log("OnInit");
-    this.crawl = data2.results[0].opening_crawl;
-    console.log("*** director ***:",this.crawl);
-    console.log("****director****",data2.results[0].director);});
-    console.log("filmID4",this.filmId4);
-
-     
+    this.crawl = data2.results[2].opening_crawl;
+    console.log("*** director 1234 ***:",this.crawl);
+    console.log("****director 3456****",data2.results[0].director);
+    console.log("filmID4",this.filmId);});
+   
       
       
 
   }
-}  
+  loadcrawl(){
+    this.filmsdata = this.http.get('https://swapi.dev/api/films');
+    this.filmsdata.subscribe(data2 =>{ console.log('my data: ', data2)
+    this.counter = this.counter +1;
+    if (this.counter >=5){this.counter=0}
+    this.crawlany = data2.results[this.counter].opening_crawl;});
+  }
 
+  
+}
